c491981643a913b6e7f94f3266540301e95d334b
Heath Mast <hrmast@gmail.com>
2020-04-08 22:35:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ def oxford_comma(array)
   elsif array.length == 3
     array.join(", ")
   else array.length > 3
-    array.join(", ")
+    str = ""
+    str << array.join(", ")
+    str << ",and "
   end
 end
